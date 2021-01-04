99984c7c4dc0a4dbd720429a0b3a95e6dfc1acae
Mario <mario@gluo.mx>
2021-01-04 13:37:46 -0600

    mas cambios al contenido

--- pages/about.js
+++ pages/about.js
@@ -42,9 +42,9 @@ function renderSingleProduct(product) {
     //'</p>' +
     '<p>' + marked(fields.descripcion) + '</p>' +
       '<div >' +
-      '<p>hola contenido' + marked(fields.contenido) + '</p>' +
-      '<p>' + marked(fields.fecha) + '</p>' +
-      JSON.stringify(fields.productosRelacionados) +
+      '<p>hola contenido' +'</p>' +
+      //'<p>' + marked(fields.fecha) + '</p>' +
+      //JSON.stringify(fields.productosRelacionados) +
       //  JSON.stringify(fields.hero)+
     '</div>' +
     //'<p>Size/Type/Color: ' + fields.sizetypecolor+ '</p>' +
