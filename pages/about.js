669953e71b9ef2e7e8422792bf7ca6d00d6305ee
Mario <mario@gluo.mx>
2021-01-04 13:27:30 -0600

    mas mapeos

--- pages/about.js
+++ pages/about.js
@@ -42,7 +42,9 @@ function renderSingleProduct(product) {
     //'</p>' +
     '<p>' + marked(fields.descripcion) + '</p>' +
       '<div >' +
-      JSON.stringify(fields.contenido) +
+      '<p>' + marked(fields.contenido) + '</p>' +
+      '<p>' + marked(fields.fecha) + '</p>' +
+      JSON.stringify(fields.productosRelacionados) +
       //  JSON.stringify(fields.hero)+
     '</div>' +
     //'<p>Size/Type/Color: ' + fields.sizetypecolor+ '</p>' +
