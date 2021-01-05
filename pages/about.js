bd486b34e52383040382caa38860ceb09cb8bf05
Mario <mario@gluo.mx>
2021-01-04 19:38:45 -0600

    mas cambios

--- pages/about.js
+++ pages/about.js
@@ -8,8 +8,13 @@ var superentry=''
  */
 
 PC.pages.about.renderHTML = function (params) {
- 
-    
+
+
+  const promesa = hazAlgo();
+  const promesa2 = promesa.then(exitoCallback, falloCallback);
+
+
+    /*
   return PC.contentfulClient
   .getEntry('6hLeTWLDy3felaFOjatheY')
   .then(entry => {
@@ -29,17 +34,36 @@ PC.pages.about.renderHTML = function (params) {
     return renderSingleProduct(superentry.fields);
   })
 
-  .catch(error => console.log(error));
+  .catch(error => console.log(error));*/
 
+} 
+
+
+function exitoCallback(resultado) { 
+  console.log("superentry fields aca");
+    console.log(superentry.fields);
+    return renderSingleProduct(superentry.fields);
+    
 }
 
+function falloCallback(error) {
+  console.log("Error: " + error);
+}
 
-
+function hazAlgo() {
+  PC.contentfulClient
+  .getEntry('6hLeTWLDy3felaFOjatheY')
+  .then(entry => {
+    superentry = entry;
+    console.log("saliendo de haz algo");
+  })
+  .catch(error => console.log(error));
+}
 
 function renderSingleProduct(entry) {
   return '<div class="product">' +
             '<div class="product-image">' +
-            '<img src="' + urlasset + '" width="300" height="300" />'
+            //'<img src="' + urlasset + '" width="300" height="300" />'
             '</div>' +
           '<div class="product-header">' +
             '<h2>' + entry.titulo + '</h2>' +
