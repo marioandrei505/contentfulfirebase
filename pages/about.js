df9ed65f462113f4f3798c5f7d9fa4fac8fd6535
Mario <mario@gluo.mx>
2021-01-13 17:21:35 -0600

    nuevos cambios

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,97 @@
 (function () {
 
+  PC.pages.about = {}
+  
+  /**
+   * Renders the products list page
+   *
+   * The products list page can optionally be filtered by a category, and will
+   * then only show products from that category. This is only used from the
+   * categories page, in order to render lists of products with only products
+   * from a selected category.
+   */
+  
+  PC.pages.about.renderHTML = function (params) {
+    var query = {
+      content_type: PC.config.articuloContentTypeId
+    }
+  
+    if (params && params.categoryId) {
+      query['fields.categories.sys.id[in]'] = params.categoryId
+    }
+  
+    return PC.contentfulClient.getEntries(query)
+    .then(function (entries) {
+      return renderProducts(entries.items)
+    })
+  }
+  
+  function renderProducts(products) {
+    return '<h1>Productsss</h1>' +
+      '<div class="products">' +
+      products.map(renderSingleProduct).join('\n') +
+      '</div>'
+  }
+  
+  function renderSingleProduct(product) {
+    var fields = product.fields
+    return '<div class="product-in-list">' +
+      '<div class="product-image">' +
+        renderImage(fields.image[0], fields.slug) +
+      '</div>' +
+      '<div class="product-details">' +
+        renderProductDetails(fields) +
+      '</div>' +
+    '</div>'
+  }
+  
+  function renderProductDetails(fields) {
+    return renderProductHeader(fields) +
+      '<p class="product-categories">' +
+      fields.categories.map(function (category) {
+        return category.fields.title
+      }).join(', ') +
+      '</p>' +
+      PC.utils.truncate(marked(fields.productDescription), 100) +
+      '<p>' + fields.price + ' &euro;</p>' +
+      '<p class="product-tags"><span>Tags:</span> ' + fields.tags.join(', ')+ '</p>'
+  }
+  
+  function renderProductHeader(fields) {
+    return '<div class="product-header">' +
+      '<h2>' +
+        '<a href="product/' + fields.slug + '" data-nav>' +
+          fields.productName +
+        '</a>'+
+      '</h2>' +
+      ' by ' +
+      '<a href="brand/' + fields.brand.sys.id + '" data-nav>' + fields.brand.fields.companyName + '</a>' +
+    '</div>'
+  }
+  
+  function renderImage(image, slug) {
+    if(image && image.fields.file) {
+      return '<a href="product/' + slug + '" data-nav>' +
+        '<img src="' + image.fields.file.url + '" width="150" height="150" />' +
+      '</a>'
+    } else {
+      return ''
+    }
+  }
+  
+  }());
+  
+
+/*
+(function () {
+
 PC.pages.about = {}
 var urlasset=''
 var superentry=''
 /**
  * Renders an article, to do all articles
  */
-
+/*
 PC.pages.about.renderHTML = function (params) {
 
 
@@ -34,6 +119,7 @@ PC.pages.about.renderHTML = function (params) {
   })
 
   .catch(error => console.log(error));*/
+  /*
 
 } 
 
@@ -99,3 +185,4 @@ function renderSingleProduct(entry) {
 
 
 
+*/
